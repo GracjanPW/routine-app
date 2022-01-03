d24bb86038f62585cd94a9b480cc1fbd47e90456
GracjanPW <40919569+GracjanPW@users.noreply.github.com>
2022-01-03 18:43:00 +0000

    save changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+interface Task {
+  todo: string,
+  done: boolean
+}
+interface TodoProps {
+  task: Task,
+  index: number,
+  check: (index: number) => void
+}
+
+interface AddFormProps {
+  open: boolean,
+  toggle: () => void,
+  add: (todo: string) => void
+}
+
+const Todo = ({task,index,check}: TodoProps) =>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <li>{task.todo}<span onClick={()=>check(index)} className='check'>{task.done?'/':' '}</span></li>
+  )
+}
+
+const AddForm = ({open,toggle,add}: AddFormProps) =>{
+  return (
+    <div onClick={toggle} className={'add-prompt'+ (open?'':' toggle-off')}>
+        <input type="text" name='text' />
     </div>
+  )
+}
+
+export const App = () => {
+  const [tasks,setTasks] = React.useState([
+    {
+      todo: "go outside",
+      done: false,
+    },
+    {
+      todo: "go outside",
+      done: false,
+    }
+  ])
+  const [open, setOpen] = React.useState(false)
+  const toggleOff = React.useCallback(() => setOpen(false),[])
+  const check = React.useCallback((index: number) =>{
+    console.log('check')
+    let newTasks = [...tasks]
+    newTasks[index].done = !newTasks[index].done
+    console.log(newTasks[index])
+    setTasks(newTasks)
+  },[tasks]
+  )
+  const add = (todo: string) =>{
+    setTasks([...tasks,{todo,done:false}])
+  }
+
+  return (
+    <div>
+      <div className="board">
+      <header className='header'>Task List</header>
+      <span onClick={()=>setOpen(true)} className='add'>+</span>
+      <ul className='list'>
+        {tasks.map((task,index)=> <Todo key={index} task={task} index={index} check={check}/>)}
+      </ul>
+      </div>
+      <AddForm open={open} toggle={toggleOff} add={add}/>
+    </div>
+    
+
+
   );
 }
 
