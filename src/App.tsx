a16010f56c5a3432b16e5020a2e2fc50390579a0
GracjanPW <40919569+GracjanPW@users.noreply.github.com>
2022-01-05 00:36:16 +0000

    can add and remove tasks

--- src/App.tsx
+++ src/App.tsx
@@ -10,39 +10,50 @@ interface TodoProps {
   task: Task,
   index: number,
   check: (index: number) => void
+  remove: (index: number) => void
 }
 
 interface AddFormProps {
   open: boolean,
   toggle: () => void,
   add: (todo: string) => void
+  
 }
 
-const Todo = ({task,index,check}: TodoProps) =>{
+
+const Todo = ({task,index,check,remove}: TodoProps) =>{
   return (
-    <li>{task.todo}<span onClick={()=>check(index)} className='check'>{task.done?'/':' '}</span></li>
+    <li>{task.todo}
+      <span className='right'>
+        <span onClick={()=>check(index)} className='check click'>{task.done?'/':' '}</span>
+        
+        <span onClick={()=>remove(index)} className='click'> [X]</span>
+      </span>
+    </li>
   )
 }
 
 const AddForm = ({open,toggle,add}: AddFormProps) =>{
+  const [text, setText] = React.useState('')
+  const close = (event:React.MouseEvent) =>{
+    if (event.target === event.currentTarget){
+      toggle()
+    }
+  }
+  const add_tgl = () => {add(text);toggle()}
   return (
-    <div onClick={toggle} className={'add-prompt'+ (open?'':' toggle-off')}>
-        <input type="text" name='text' />
+    <div onClick={close} className={'add-prompt'+ (open?'':' toggle-off')}>
+        <div className='add-box'>
+          <input type="text" name='text' value={text} onChange={(e)=>setText(e.target.value)}/>
+          <input type="button" value="Add" onClick={()=>text.length>0?add_tgl():alert('no task was entered')}/>
+        </div>
+        
     </div>
   )
 }
 
 export const App = () => {
-  const [tasks,setTasks] = React.useState([
-    {
-      todo: "go outside",
-      done: false,
-    },
-    {
-      todo: "go outside",
-      done: false,
-    }
-  ])
+  const [tasks,setTasks] = React.useState<Task[]>([])
   const [open, setOpen] = React.useState(false)
   const toggleOff = React.useCallback(() => setOpen(false),[])
   const check = React.useCallback((index: number) =>{
@@ -56,14 +67,21 @@ export const App = () => {
   const add = (todo: string) =>{
     setTasks([...tasks,{todo,done:false}])
   }
+  const remove = (index:number) =>{
+    setTasks(prev=>{
+      let tasks = [...prev]
+      tasks.splice(index,1)
+      return tasks
+    })
+  }
 
   return (
     <div>
       <div className="board">
       <header className='header'>Task List</header>
-      <span onClick={()=>setOpen(true)} className='add'>+</span>
+      <span onClick={()=>setOpen(true)} className='add click'>+</span>
       <ul className='list'>
-        {tasks.map((task,index)=> <Todo key={index} task={task} index={index} check={check}/>)}
+        {tasks.map((task,index)=> <Todo key={index} task={task} index={index} check={check} remove={remove}/>)}
       </ul>
       </div>
       <AddForm open={open} toggle={toggleOff} add={add}/>
